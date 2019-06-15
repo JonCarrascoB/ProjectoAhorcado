53df4d349dc4c68d742f079043d454d1fd1e065a
Jon Carrasco <jcarrascobar@gmail.com>
2019-06-15 12:36:32 +0200

    commit 3 excepciones controladas

--- Ahorcado/src/JuegoAhorcado.java
+++ Ahorcado/src/JuegoAhorcado.java
@@ -8,9 +8,18 @@ public class JuegoAhorcado {
 		int vidas = 7;
 		int aciertos = 0;
 		int cont = 0;
+		
+		// Pruebas de desarrollo con una palabra fija
+		// String palabra = "viernes";
 
-		String palabra = "viernes";
-		char[] pal = palabra.toCharArray();
+		// creamos un arry de palabras que salgan al azar, para dar más dinamismo al
+		// juego
+		String[] palabras = new String[] { "emperador", "arcipreste", "almirante", "comodoro", "sacerdote",
+				"camposanto", "pelmazo" };
+
+		// Insertamos un random para que las palabras salgan al azar
+		String palabra = palabras[(int) (Math.random() * (palabras.length - 1))];
+
 		int tamano = palabra.length();
 
 		char[] respuesta = new char[tamano];
@@ -30,31 +39,55 @@ public class JuegoAhorcado {
 		do {
 
 			System.out.println("Inserte una letra: ");
-			String letra = sc.nextLine();
 			
+			// Introducimos esta excepcion por si se pulsa intro sin meter ninguna letra
+			try {
+				String letra = sc.nextLine();
 
-			// busca si contiene la letra introducida en la palabra
-			if (palabra.contains(letra)) {
-				for (int i = 0; i < tamano; i++) {
-					if (palabra.charAt(i) == letra.charAt(0) && respRep[i].equals(false)) {
-						respuesta[i] = letra.charAt(0);
-						respRep[i] = true;
-						cont++;
+				// Ahora comprobamos que se ha metido un string de un caracter (letra) 
+				// y no una cadena de valores
+				if (letra.length() <= 1) {
+
+					// Ahora obviamos que metan ningun otro tipo de caracter que no se una letra
+					// y de esa forma que no corran vidas
+					if (Character.isLetter(letra.charAt(0))) {
+
+						// Ahora impedimos que al usar una mayuscula pierda una vida
+						if (Character.isLowerCase(letra.charAt(0))) {
+
+							// busca si contiene la letra introducida en la palabra
+							if (palabra.contains(letra)) {
+								for (int i = 0; i < tamano; i++) {
+									if (palabra.charAt(i) == letra.charAt(0) && respRep[i].equals(false)) {
+										respuesta[i] = letra.charAt(0);
+										respRep[i] = true;
+										cont++;
+									}
+
+								}
+
+								aciertos = aciertos + cont; // acierta
+							} else {
+								vidas = vidas - 1; // pierde vida
+							}
+							cont = 0;
+							dibujo(vidas); // Llama a un metodo con los dibujos pasando el numero de vidas
+							System.out.println("Tus vidas son: " + vidas + "/7."); // controla que pierdes vida
+							for (int i = 0; i < tamano; i++) {
+								System.out.println("---- " + respuesta[i] + " ------ ");
+
+							}
+						} else {
+							System.out.println("Debe introducir una letra minuscula, pruebe otra vez");
+						}
+					} else {
+						System.out.println("Si no introduces una letra, no puedes jugar, prueba otra vez");
 					}
-
+				} else {
+					System.out.println("Se debe introducir una letra, no una palabra, pruebe otra vez");
 				}
-
-				aciertos = aciertos + cont; // acierta
-			} else {
-				vidas = vidas - 1; // pierde vida
-				dibujo(vidas); // Llama a un metodo con los dibujos pasando el numero de vidas
-				System.out.println("Tus vidas son: " + vidas + "/7."); // controla que pierdes vida
-			}
-			cont = 0;
-
-			for (int i = 0; i < tamano; i++) {
-				System.out.println("---- " + respuesta[i] + " ------ ");
-				
+			} catch (StringIndexOutOfBoundsException e) {
+				System.out.println("Debe introducir algo");
 			}
 
 		} while (vidas != 0 && aciertos != tamano);
